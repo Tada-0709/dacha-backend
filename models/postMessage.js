ce75f9bd1b04552a5a4548c4910fd776448f8874
Tada-0709 <datlxag1998@gmail.com>
2021-06-14 18:06:38 +0700

    Init: base folders

---
+++ models/postMessage.js
@@ -0,0 +1,22 @@
+import Mongoose from "mongoose";
+
+const postSchema = Mongoose.Schema({
+  title: String,
+  message: String,
+  creator: String,
+  tags: [String],
+  likeCount: {
+    type: Number,
+    default: 0,
+  },
+  createdAt: {
+    type: Date,
+    default: new Date(),
+  },
+});
+
+//Convert a schema to model
+
+const PostMessage = Mongoose.model("PostMessage", postSchema);
+
+export default PostMessage;
