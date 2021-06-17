ec0b731a05fd8a894797c295c109b9ec26a0b143
Nguyen Ho Tat Dat <45982145+Tada-0709@users.noreply.github.com>
2021-06-17 10:55:30 +0700

    deploy: heroku

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,3 +1,4 @@
+import Mongoose from "mongoose";
 import PostMessage from "../models/postMessage.js";
 
 export const getPosts = async (req, res) => {
@@ -20,4 +21,46 @@ export const createPost = async (req, res) => {
   } catch (error) {
     res.status(409).json({ message: error.message });
   }
+};
+
+export const updatePost = async (req, res) => {
+  const { id: _id } = req.params;
+  const post = req.body;
+  console.log(`Received update request id: ${_id}`);
+
+  validateMongoObjectId(_id, res);
+
+  const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, {
+    new: true,
+  });
+  res.json(updatedPost);
+};
+
+export const deletePost = async (req, res) => {
+  const { id: _id } = req.params;
+  console.log(`Delete post with id: ${_id}`);
+
+  validateMongoObjectId(_id, res);
+  await PostMessage.findByIdAndDelete(_id);
+
+  res.json({ message: "Post deleted." });
+};
+
+export const likePost = async (req, res) => {
+  const { id: _id } = req.params;
+  console.log(`New like for post with id: ${_id}`);
+  validateMongoObjectId(_id, res);
+
+  const post = await PostMessage.findById(_id);
+  const updatedPost = await PostMessage.findByIdAndUpdate(
+    _id,
+    { likeCount: post.likeCount + 1 },
+    { new: true }
+  );
+  res.json(updatedPost);
+};
+
+const validateMongoObjectId = (_id, res) => {
+  if (!Mongoose.Types.ObjectId.isValid(_id))
+    return res.status(404).send("No post with that id");
 };
--- index.js
+++ index.js
@@ -2,10 +2,12 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+import dotenv from "dotenv";
 
 import postRoutes from "./routes/posts.js";
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -13,12 +15,14 @@ app.use(cors());
 
 app.use("/posts", postRoutes);
 
-const CONNECTIONS_URL =
-  "mongodb+srv://steven328:dat328@cluster0.6isjs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+app.get("/", (req, res) => {
+  res.send("Hello to DanCha API");
+});
+
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(CONNECTIONS_URL, {
+  .connect(process.env.CONNECTIONS_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
